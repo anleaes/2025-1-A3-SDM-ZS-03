e23a519cb59af568aa68d3f6a44e77a65579a139
lvsmendes <lvsmendes2@gmail.com>
2025-06-10 18:22:39 -0300

    adicionado o caminho da entidade apos o path

--- projetoapp/urls.py
+++ projetoapp/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuario.urls', namespace='usuario')),
    
 ]
 
