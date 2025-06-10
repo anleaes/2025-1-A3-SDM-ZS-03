b3b1ff7fe424f60f21a5117bac34e90ab9e7fd1d
lvsmendes <lvsmendes2@gmail.com>
2025-06-10 18:02:54 -0300

    classe meta adicionada no models

--- projetoapp/apps/usuario/models.py
+++ projetoapp/apps/usuario/models.py
@@ -12,3 +12,11 @@ class Usuario(models.Model):
     gender = models.CharField('Gênero', max_length=1, choices=GENDER_CHOICES)
     address = models.CharField('Endereço', max_length=255)
     phone = models.CharField('Telefone', max_length=15)
+
+class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['id']
+
+def __str__(self):
+     return f"{self.first_name} {self.last_name}"
