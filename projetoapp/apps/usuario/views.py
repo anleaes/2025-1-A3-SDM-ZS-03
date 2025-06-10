cf742c74248f9190a90fd97a169ef111bb29d06e
lvsmendes <lvsmendes2@gmail.com>
2025-06-10 18:16:57 -0300

    views criada em usuario

--- projetoapp/apps/usuario/views.py
+++ projetoapp/apps/usuario/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Usuario
 from .serializers import UsuarioSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
