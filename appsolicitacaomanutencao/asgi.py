2360320cc00e0dbd6c83eee9b546b2f53e6288d0
lvsmendes <lvsmendes2@gmail.com>
2025-06-11 17:30:46 -0300

    toda a configuracao do django

--- appsolicitacaomanutencao/asgi.py
+++ appsolicitacaomanutencao/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for projetoapp project.
+ASGI config for appsolicitacaomanutencao project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appsolicitacaomanutencao.settings")
 
 application = get_asgi_application()
--- projetoapp/apps/usuario/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Usuario
-from .serializers import UsuarioSerializer
-
-class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
