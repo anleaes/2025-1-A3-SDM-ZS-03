fcaf8092c713320fa7e02650c12efea33dc254b0
lvsmendes <lvsmendes2@gmail.com>
2025-06-11 18:16:57 -0300

    class usuario criada no models

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Usuario(models.Model):
+    first_name = models.CharField('Primeiro Nome', max_length=50)
+    last_name = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', null=False, blank=False)
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    )
+    gender = models.CharField('Gênero', max_length=1, choices=GENDER_CHOICES)
+    address = models.CharField('Endereço', max_length=255)
+    phone = models.CharField('Telefone', max_length=15)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['id']
