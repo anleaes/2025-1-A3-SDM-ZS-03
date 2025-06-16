3b61ee113a00aabe1f95665b7a5d1f41960ed0c6
lvsmendes <lvsmendes2@gmail.com>
2025-06-16 19:08:41 -0300

    importes feitos no views

--- apps/solicitacao/views.py
+++ apps/solicitacao/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import SolicitacaoTemporaria
+from .serializers import SolicitacaoTemporariaSerializer
 
 # Create your views here.
