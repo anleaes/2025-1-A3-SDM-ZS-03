16cdb02c78c8ec68e5c61a68e1998ee2f25b0fd4
lvsmendes <lvsmendes2@gmail.com>
2025-06-17 18:27:57 -0300

    import feito em views

--- apps/itemsolicitacao/views.py
+++ apps/itemsolicitacao/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ItemSolicitacao
+from .serializers import ItemSolicitacaoSerializer
 
 # Create your views here.
