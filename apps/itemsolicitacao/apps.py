08a5127423a74fa6b1528486e3f4f0d04dee9e35
lvsmendes <lvsmendes2@gmail.com>
2025-06-17 18:17:07 -0300

    adicionado verbose name no apps

--- apps/itemsolicitacao/apps.py
+++ apps/itemsolicitacao/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ItemsolicitacaoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "itemsolicitacao"
+    verbose_name = 'Itens de Solicitação'
