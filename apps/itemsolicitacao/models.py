da71c024a39909133d90f0f315cd9aae9d0bc493
lvsmendes <lvsmendes2@gmail.com>
2025-06-17 18:19:42 -0300

    models criado

--- apps/itemsolicitacao/models.py
+++ apps/itemsolicitacao/models.py
@@ -4,3 +4,15 @@ from solicitacao.models import SolicitacaoTemporaria
 
 
 # Create your models here.
+class ItemSolicitacao(models.Model):
+    solicitacao = models.ForeignKey(SolicitacaoTemporaria, on_delete=models.CASCADE, related_name='itens')
+    descricao_problema = models.TextField('Descrição do problema')
+    quantidade = models.PositiveIntegerField('Quantidade', default=1)
+
+    class Meta:
+        verbose_name = 'Item de Solicitação'
+        verbose_name_plural = 'Itens de Solicitação'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"Item #{self.id} - Solicitação {self.solicitacao.id}"
