6a9d1e91ceb4a9d7ae915c37c5c97652c345a15e
lvsmendes <lvsmendes2@gmail.com>
2025-06-17 18:11:32 -0300

    adicionado ao urls na main

--- appsolicitacaomanutencao/urls.py
+++ appsolicitacaomanutencao/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('solicitacoes/', include('solicitacao.urls', namespace='solicitacao')),
+
 ]
