4ddd84755059e3c413b62d6037899f176075c7a8
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 10:55:09 -0300

    imports feitos em models

--- apps/chamadomanutencao/models.py
+++ apps/chamadomanutencao/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db import models
+from solicitacao.models import SolicitacaoTemporaria
+from usuario.models import Usuario  # Técnico
+from local.models import Local  # se existir
 
 # Create your models here.
