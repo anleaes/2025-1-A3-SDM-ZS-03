4b83c4d92dc63a00619a3ff539e82c5d2546f252
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:44:23 -0300

    urls criado

--- apps/historicoatividade/urls.py
+++ apps/historicoatividade/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import HistoricoAtividadeViewSet
+
+app_name = 'historicoatividade'
+
+router = routers.DefaultRouter()
+router.register('', HistoricoAtividadeViewSet, basename='historico')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
