964bf0dea27c716982e6c605082d7095e87e012f
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:13:26 -0300

    urls configurado

--- apps/chamadomanutencao/urls.py
+++ apps/chamadomanutencao/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ChamadoManutencaoViewSet
+
+app_name = 'chamadomanutencao'
+
+router = routers.DefaultRouter()
+router.register('', ChamadoManutencaoViewSet, basename='chamados')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
