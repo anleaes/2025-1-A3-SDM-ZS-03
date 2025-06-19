b6f89cf1e8f4fc04844f51483ec855235e0b9f5a
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 12:21:11 -0300

    views criado

--- apps/avaliacao/views.py
+++ apps/avaliacao/views.py
@@ -4,3 +4,7 @@ from .models import Avaliacao
 from .serializers import AvaliacaoSerializer
 
 # Create your views here.
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
