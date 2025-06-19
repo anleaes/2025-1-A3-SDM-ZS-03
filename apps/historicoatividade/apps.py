db129a0c6d562299a639024461998b8a422e5e43
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:36:35 -0300

    adicionado verbose name ao apps

--- apps/historicoatividade/apps.py
+++ apps/historicoatividade/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HistoricoatividadeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "historicoatividade"
+    verbose_name = 'Histórico de Atividades'
