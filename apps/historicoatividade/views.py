406c83b4d6278e319f1ed34a7638c9b4cbdea77c
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:41:38 -0300

    import feitos em views

--- apps/historicoatividade/views.py
+++ apps/historicoatividade/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import HistoricoAtividade
+from .serializers import HistoricoAtividadeSerializer
 # Create your views here.
