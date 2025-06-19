ecf04338ef85a6ccab18af9b067b63b5861abe7e
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:10:41 -0300

    views criado

--- apps/chamadomanutencao/views.py
+++ apps/chamadomanutencao/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .models import ChamadoManutencao
 from .serializers import ChamadoManutencaoSerializer
 # Create your views here.
+class ChamadoManutencaoViewSet(viewsets.ModelViewSet):
+    queryset = ChamadoManutencao.objects.all()
+    serializer_class = ChamadoManutencaoSerializer
