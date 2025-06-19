fef3dad67d1f17b3d58e37302be1a09641adf683
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 11:10:17 -0300

    import feito em views

--- apps/chamadomanutencao/views.py
+++ apps/chamadomanutencao/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import ChamadoManutencao
+from .serializers import ChamadoManutencaoSerializer
 # Create your views here.
