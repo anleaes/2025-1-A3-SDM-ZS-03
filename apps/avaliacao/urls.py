1d99d95b2d66bd418bc9d0f89413f27db5c488f5
lvsmendes <lvsmendes2@gmail.com>
2025-06-19 12:22:54 -0300

    urls criado

--- apps/avaliacao/urls.py
+++ apps/avaliacao/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import AvaliacaoViewSet
+
+app_name = 'avaliacao'
+
+router = routers.DefaultRouter()
+router.register('', AvaliacaoViewSet, basename='avaliacoes')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
