302b563bc3ff109320e8940ad37d9c1bc9ac1430
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 18:11:58 -0300

    classe local criada

--- apps/local/models.py
+++ apps/local/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Local(models.Model):
+    nome = models.CharField('Nome do Local', max_length=100)
+    bloco = models.CharField('Bloco', max_length=50, blank=True, null=True)
+    andar = models.CharField('Andar', max_length=20, blank=True, null=True)
+    sala = models.CharField('Sala', max_length=50, blank=True, null=True)
+    referencia = models.TextField('Referência/Observações', blank=True, null=True)
