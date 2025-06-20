1f42d6b84a911e6b901021557d43123626d915d0
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 17:56:13 -0300

    serializer criado

---
+++ apps/tecnico/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Tecnico
+
+class TecnicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tecnico
+        fields = '__all__'
