defbffe1efc8a91cfcae0e0aaa6692ff10f9933b
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 19:00:25 -0300

    import feito em views

--- apps/atribuicaochamado/views.py
+++ apps/atribuicaochamado/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AtribuicaoChamado
+from .serializers import AtribuicaoChamadoSerializer
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
