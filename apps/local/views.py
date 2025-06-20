47f4f398a384eff4f054ba876c0ea6297f075e90
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 18:15:50 -0300

    views criado

--- apps/local/views.py
+++ apps/local/views.py
@@ -4,3 +4,6 @@ from .models import Local
 from .serializers import LocalSerializer
 
 # Create your views here.
+class LocalViewSet(viewsets.ModelViewSet):
+    queryset = Local.objects.all()
+    serializer_class = LocalSerializer
