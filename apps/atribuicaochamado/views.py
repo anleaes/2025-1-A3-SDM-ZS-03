14e7aff149dcd4789fbb2ad4cdf40f9d1a0dc44b
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 19:00:52 -0300

    views criado

--- apps/atribuicaochamado/views.py
+++ apps/atribuicaochamado/views.py
@@ -5,3 +5,10 @@ from .serializers import AtribuicaoChamadoSerializer
 from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+class AtribuicaoChamadoViewSet(viewsets.ModelViewSet):
+    queryset = AtribuicaoChamado.objects.all()
+    serializer_class = AtribuicaoChamadoSerializer
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        serializer.save(atribuido_por=self.request.user)
