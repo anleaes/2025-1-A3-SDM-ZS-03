8b393ea013f3ed9117a2a04929dbb862a48f3c04
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 18:03:52 -0300

    urls adicionado na classe principal

--- appsolicitacaomanutencao/urls.py
+++ appsolicitacaomanutencao/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('usuarios/', include('usuario.urls', namespace='usuario')),
     path('historico/', include('historicoatividade.urls', namespace='historicoatividade')),
     path('avaliacoes/', include('avaliacao.urls', namespace='avaliacao')),
+    path('tecnicos/', include('tecnico.urls', namespace='tecnico')),
+
 
 
 
