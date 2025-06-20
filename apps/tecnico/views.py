8c0d74eab2596a1c042b3ce110173cd2296cbefc
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 17:57:41 -0300

    views criado

--- apps/tecnico/views.py
+++ apps/tecnico/views.py
@@ -4,3 +4,6 @@ from .models import Tecnico
 from .serializers import TecnicoSerializer
 
 # Create your views here.
+class TecnicoViewSet(viewsets.ModelViewSet):
+    queryset = Tecnico.objects.all()
+    serializer_class = TecnicoSerializer
