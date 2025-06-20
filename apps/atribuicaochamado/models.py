5cbb13a3d9229e281c60a721cf244694ce95022c
lvsmendes <lvsmendes2@gmail.com>
2025-06-20 18:56:11 -0300

    import feito em models

--- apps/atribuicaochamado/models.py
+++ apps/atribuicaochamado/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db import models
+from chamadomanutencao.models import ChamadoManutencao
+from tecnico.models import Tecnico
+from usuario.models import Usuario
 
 # Create your models here.
